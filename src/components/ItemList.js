2801d991c9affbb222a02f35664c876a7790b455
Your Name <harshitdagar913@gmail.com>
2026-03-02 14:18:26 +0530

    Fix link paths in ItemList component for correct routing

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -2,14 +2,15 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const ItemList = () => {
-    const items = [1, 2, 3];
+    const items = [1, 2, 3]; // Exact 3 items
   return (
     <div>
       <h1>Item List</h1>
       <ul>
-        {items.map((item, index) => (
-            <li key={index}>{item}
-                <Link to={`/item/${index}`}>Item {index}</Link>
+        {items.map((item) => (
+            <li key={item}>
+                {/* FIX: path ko '/items/' kiya aur text ko sirf 'Item {item}' rakha */}
+                <Link to={`/items/${item}`}>Item {item}</Link>
             </li>
         ))}
       </ul>
