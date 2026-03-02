e1c71b83e0721965ac62313ba661e634c84de7cc
Your Name <harshitdagar913@gmail.com>
2026-03-02 14:12:38 +0530

    Refactor App component to remove BrowserRouter and clean up code structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,19 @@
-
-import React from "react";
-import './../styles/App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ItemList from "./ItemList";
-import ItemDetail from "./ItemDetail";
+import React from 'react';
+// Hum yahan BrowserRouter nahi laga rahe taaki "Double Router" ka conflict na ho
+import { Routes, Route } from 'react-router-dom'; 
+import ItemList from './ItemList';
+import ItemDetail from './ItemDetail';
 
 const App = () => {
   return (
+    // Yahan koi extra <li>, <nav> ya faltu tags nahi hone chahiye!
     <div>
-        {/* Do not remove the main div */}
-        <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<ItemList />} />
-              <Route path="/item/:id" element={<ItemDetail />} />
-            </Routes>
-        </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ItemList />} />
+        <Route path="/items/:id" element={<ItemDetail />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
